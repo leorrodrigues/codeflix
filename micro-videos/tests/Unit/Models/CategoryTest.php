721b217d16ab74e5afbf6ac05db1c78151feabf5
leonardo rodrigues <leu_rr1@hotmail.com>
2020-06-25 22:32:52 -0300

    New Tests

---
+++ micro-videos/tests/Unit/Models/CategoryTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Tests\Unit\Models;
+
+use App\Models\Category;
+use App\Models\Traits\Uuid;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use PHPUnit\Framework\TestCase;
+
+class CategoryTest extends TestCase
+{
+    protected function setUp(): void {
+        parent::setUp();
+        $this->category = new Category();
+    }
+
+    public function testIfUseTraits(){
+        $traits = [
+            Uuid::class,
+            SoftDeletes::class
+        ];
+        $categoryTraits = array_keys(class_uses(Category::class));
+        $this->assertEqualsCanonicalizing($traits, $categoryTraits);
+    }
+
+    public function testFillableAttribute()
+    {
+        $fillable = ['name', 'description', 'is_active'];
+        $this->assertEquals($fillable, $this->category->getFillable());
+    }
+
+    public function testCastsAttribute()
+    {
+        $casts = ['id' => 'string', 'is_active' => 'boolean'];
+        $this->assertEquals($casts, $this->category->getCasts());
+    }
+
+    public function testIncrementingAttribute()
+    {
+        $this->assertFalse($this->category->incrementing);
+    }
+
+    public function testDatesAttribute()
+    {
+        $dates = ['deleted_at', 'created_at', 'updated_at'];
+
+        $this->assertEqualsCanonicalizing($dates, $this->category->getDates());
+    }
+}
