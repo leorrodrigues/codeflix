d10b07bf886c6171b8afdec197b166e2aa33ef0a
leonardo rodrigues <leu_rr1@hotmail.com>
2020-06-25 22:04:42 -0300

    New Tests

--- micro-videos/routes/api.php
+++ micro-videos/routes/api.php
@@ -18,10 +18,6 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 });
 
 Route::group(['namespace' => 'Api'], function () {
-    $exceptCreateAndEdit = [
-        'except' => ['create', 'edit']
-    ];
-
-    Route::resource('categories', 'CategoryController', $exceptCreateAndEdit);
-    Route::resource('genres', 'GenreController', $exceptCreateAndEdit);
+    Route::resource('categories', 'CategoryController', ['except' => ['create', 'edit']]);
+    Route::resource('genres', 'GenreController', ['except' => ['create', 'edit']]);
 });
