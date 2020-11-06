d045f4d8a3162108fd7e37835f8850077312090d
leonardo rodrigues <leu_rr1@hotmail.com>
2020-11-06 19:17:19 -0300

    Added withTrashed in relations and updated the seeds

--- database/seeds/GenresTableSeeder.php
+++ database/seeds/GenresTableSeeder.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use App\Models\Category;
+use App\Models\Genre;
 
 class GenresTableSeeder extends Seeder
 {
@@ -11,6 +13,12 @@ class GenresTableSeeder extends Seeder
      */
     public function run()
     {
-        Factory(\App\Models\Genre::class, 20)->create();
+        $categories = Category::all();
+        Factory(\App\Models\Genre::class, 100)
+            ->create()
+            ->each(function(Genre $genre) use($categories){
+                $categoriesId = $categories->random(5)->pluck('id')->toArray();
+                $genre->categories()->attach($categoriesId);
+            });
     }
 }
